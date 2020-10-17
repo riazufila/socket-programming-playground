48f1b462bac489de39abc40665f1748ec9edc250
riazufila <46749569+riazufila@users.noreply.github.com>
2020-10-17 21:47:26 +0800

    Corrected userinput.c

--- userinput.c
+++ userinput.c
@@ -5,7 +5,7 @@ int main(void) {
     int age;
     printf("Hi, how old are you? > ");
     scanf("%d", &age);
-    printf("You are &d years old", age);
+    printf("You are %d years old", age);
 
     return(0);
 }
