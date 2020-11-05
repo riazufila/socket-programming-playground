0b70eb09b704fb266d49ccf028c9d0d980a0323d
riazufila <46749569+riazufila@users.noreply.github.com>
2020-11-05 23:05:02 +0800

    Renamed file

--- application/client.c
+++ application/client.c
@@ -6,34 +6,35 @@
 #include <sys/types.h>
 
 int main(int argc, char *argv[]) {
-    int socket_desc;
-    struct sockaddr_in server;
+    while(1) {
+        int socket_desc;
+        struct sockaddr_in server;
 
-    // Create socket
-    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-    if(socket_desc < 0)
-        perror("socket()");
+        // Create socket
+        socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+        if(socket_desc < 0)
+            perror("socket()");
 
-    server.sin_addr.s_addr = inet_addr("192.168.42.84");
-    server.sin_family = AF_INET;
-    server.sin_port = htons( 4545 );
+        server.sin_addr.s_addr = inet_addr("192.168.42.84");
+        server.sin_family = AF_INET;
+        server.sin_port = htons( 4545 );
 
-    // Connect to remote server
-    if(connect(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        perror("connect()");
+        // Connect to remote server
+        if(connect(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) {
+            perror("connect()");
 
-        return 1;
+            return 1;
+        }
+
+        // Send some date
+        char message[2000];
+        fgets(message, 2000, stdin);
+        send(socket_desc, message, 2000, 0);
+
+        // Receive data
+        char buffer[2000];
+        recv(socket_desc, buffer, 2000, 0);
+        printf("SERVER: %s\n", buffer);
     }
-
-    // Send some date
-    char message[2000];
-    fgets(message, 2000, stdin);
-    send(socket_desc, message, 2000, 0);
-
-    // Receive data
-    char buffer[2000];
-    recv(socket_desc, buffer, 2000, 0);
-    printf("SERVER: %s\n", buffer);
-
     return 0;
 }
