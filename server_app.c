fc2ac54954e5dda15bb95b13251e6e2e8be358e3
riazufila <46749569+riazufila@users.noreply.github.com>
2020-11-05 11:03:00 +0800

    Server Application

---
+++ server_app.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]) {
+    int socket_desc, new_socket, c;
+    struct sockaddr_in server, client;
+
+    // Create socket
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if(socket_desc == -1)
+        printf("Could not create socket.");
+
+    // Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(8888);
+
+    // Bind the socket
+    if(bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0)
+        puts("Bind failed.");
+
+    puts("Binding completed.");
+
+    // Listen for connection
+    listen(socket_desc, 3);
+
+    // Accept incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+    new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+
+    if(new_socket < 0)
+        perror("Accepting Connection Failed");
+
+    return 0;
+}
