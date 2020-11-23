5d1df3a6440e96141a0d916ce430924716454179
riazufila <46749569+riazufila@users.noreply.github.com>
2020-11-23 21:49:05 +0800

    QOTD server side boilerplate

---
+++ QOTD-server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import socket
+
+PORT = 17  # Set default port for QOTD
+MSG = 'The world is ending..'
+
+# Create socket
+try:
+    SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error as socket_error:
+    print("Socket failed to create with error %s" % (socket_error))
+
+# Bind socket
+try:
+    SOCKET.bind(('', PORT))
+except socket.error as socket_error:
+    print("Socket failed to bind with error %s" % (socket_error))
+
+while SOCKET:
+    print("Ready to receive connections..")
+    # based on RFC for QOTD Protocol the size must not exceed 512 bytes
+    data, addr = SOCKET.recvfrom(512)
+    print("Connection established from %d" % (addr))
+    # Data received is discarded
+    SOCKET.sendto(MSG, (addr, PORT))
