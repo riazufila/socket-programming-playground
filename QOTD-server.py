d2b75e719f9c354557488d7c5e94f1426db46084
riazufila <46749569+riazufila@users.noreply.github.com>
2020-11-25 16:55:16 +0800

    Retrieve quote

--- QOTD-server.py
+++ QOTD-server.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import socket
+from datetime import datetime
 
 PORT = 17  # Set default port for QOTD
-MSG = b"The world is ending.."
 
 # Create socket
 try:
@@ -18,10 +18,37 @@ except socket.error as socket_error:
     print("Socket failed to bind with error %s" % (socket_error))
 
 while SOCKET:
-    print("Ready to receive connections..")
-    # based on RFC for QOTD Protocol the size must not exceed 512 bytes
-    data, addr = SOCKET.recvfrom(512)
-    print("Connection established from %s" % str(addr[0]))
-    # Data received is discarded
-    # Send QOTD to client
-    SOCKET.sendto(MSG, addr)
+    # Check for new day
+    day = datetime.now()
+
+    try:
+        print("Ready to receive connections..")
+        # based on RFC for QOTD Protocol the size must not exceed 512 bytes
+        data, addr = SOCKET.recvfrom(512)
+        print("Connection established from %s" % str(addr[0]))
+        # Data received is discarded
+    except socket.error as socket_error:
+        print("Socket failed to receive data with error %s" % (socket_error))
+
+    try:
+        # Get QOTD
+        f = open("quotes.txt", "r")
+
+        day_buffer = datetime.now()
+        day_differences = day_buffer - day
+        day_differences = str(day_differences)
+        day_differences = day_differences.split(" ")[0]
+        day_diff_len = len(day_differences)
+
+        if day_diff_len == 1:
+            MSG = f.readline().encode("utf-8")
+        else:
+            i = day_differences[0]
+            for x in range(i + 1):
+                MSG = f.readline().encode("utf-8")
+
+        # Send QOTD to client
+        SOCKET.sendto(MSG, addr)
+        print("Sent Quote to %s" % (str(addr[0])))
+    except socket.error as socket_error:
+        print("Socket failed to send data with error %s" % (socket_error))
