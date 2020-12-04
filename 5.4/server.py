f3f28bcad99e4d4ad3cb9e234a58af5d050877e6
riazufila <46749569+riazufila@users.noreply.github.com>
2020-12-05 04:22:05 +0800

    AES encryption on server

--- 5.4/server.py
+++ 5.4/server.py
@@ -5,7 +5,7 @@ from Crypto.Cipher import AES
 
 
 def decrypt(encrypted_data):
-    obj = AES.new(b"This is a key123", AES.MODE_CBC, b"This is an IV456")
+    obj = AES.new(b"1234567890123456", AES.MODE_CFB, b"0987654321098765")
     data = obj.decrypt(encrypted_data)
 
     return data
@@ -26,7 +26,7 @@ while True:
     RecvData = conn.recv(1024)
     RecvData = decrypt(RecvData)
     while RecvData:
-        file.write(RecvData.decode("utf-8"))
+        file.write(RecvData)
         RecvData = conn.recv(1024)
         RecvData = decrypt(RecvData)
 
