51a741ced12769360270640d50031a5d6b8e6368
riazufila <46749569+riazufila@users.noreply.github.com>
2020-12-05 05:08:28 +0800

    Thread increment

--- 5.2/server.py
+++ 5.2/server.py
@@ -34,7 +34,7 @@ while True:
     Client, address = ServerSocket.accept()
     print("Connected to:" + address[0] + ":" + str(address[1]))
     start_new_thread(threaded_client, (Client, ))
-    ThreadCount = 1
     print("Thread Number:" + str(ThreadCount))
+    ThreadCount = ThreadCount + 1
 
 ServerSocket.close()
