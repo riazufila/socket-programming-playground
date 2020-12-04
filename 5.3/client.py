6ebb4040b77c4a1612ac24fdf057e35a70821861
riazufila <46749569+riazufila@users.noreply.github.com>
2020-12-05 01:24:25 +0800

    Client app with JSON

---
+++ 5.3/client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import socket
+import sys
+import json
+
+s = socket.socket()
+
+port = 8080
+
+s.connect(("192.168.8.112", port))
+
+data = s.recv(1024)
+data = data.decode("utf-8")
+
+s.send(b'Thank you from client')
+
+dataJ = json.loads(data)
+
+print(type(dataJ))
+print(dataJ)
+
+s.close()
