3db7487569742098bb15b67bb62cc20b9b25d2cc
riazufila <46749569+riazufila@users.noreply.github.com>
2020-12-05 04:41:43 +0800

    Same filename copied

--- 5.4/client.py
+++ 5.4/client.py
@@ -2,7 +2,9 @@
 
 import os
 import socket
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES  # Encryption module
+
+# Encryption process
 
 
 def encryption(data):
@@ -14,15 +16,22 @@ def encryption(data):
 
 IP = "192.168.42.198"
 PORT = 4545
-FILE = input("Enter the absolute path for the file to send to serve: ")
+FILE = input(
+    "Enter the filename (must be in the same directory) to send to server: ")
 FILESIZE = os.path.getsize(FILE)
 
+# Socket creation
 s = socket.socket()
+# Socket connection
 s.connect((IP, PORT))
 
 file = open(FILE, "rb")
 SendData = file.read(1024)
 
+# Sending filename to server
+s.send(FILE.encode("utf-8"))
+
+# Padding
 length = 16 - (len(SendData) % 16)
 SendData += bytes([length])*length
 
--- 5.4/server.py
+++ 5.4/server.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 import socket
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES  # Library for encryption
 
-
+# Decryption
 def decrypt(encrypted_data):
     obj = AES.new(b"1234567890123456", AES.MODE_CFB, b"0987654321098765")
     data = obj.decrypt(encrypted_data)
@@ -11,15 +11,17 @@ def decrypt(encrypted_data):
     return data
 
 
-s = socket.socket()
-PORT = 4545
-s.bind(('', PORT))
-s.listen(10)
-
-file = open("got-hello.txt", "wb")
+s = socket.socket()  # Socket creation
+PORT = 4545  # Port defined
+s.bind(('', PORT))  # Binding socket
+s.listen(10)  # Socket waits for connections
 
 while True:
     conn, addr = s.accept()
+
+    filename = conn.recv(1024)
+    file = open(filename, "wb")
+
     msg = "Message from server"
     conn.send(msg.encode("utf-8"))
 
