e667e870c2126cbf672f5308d7493b722cbbd405
riazufila <46749569+riazufila@users.noreply.github.com>
2020-12-07 20:03:37 +0800

    main()

--- QOTD-client.py
+++ QOTD-client.py
@@ -2,16 +2,22 @@
 
 import socket
 
-PORT = 17  # Default QOTD port
-IP = b"192.168.42.69"  # Server's IP
-MSG = b"Let me in!"
 
-# Create socket
-SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def main():
+    PORT = 17  # Default QOTD port
+    IP = b"192.168.42.69"  # Server's IP
+    MSG = b"Let me in!"
 
-# Send datagram to Server for QOTD
-SOCKET.sendto(MSG, (IP, PORT))
+    # Create socket
+    SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Receive datagram from Server
-data, addr = SOCKET.recvfrom(512)  # 512 bytes as recommended in RFC
-print("QOTD: %s" % (data.decode("utf-8")))
+    # Send datagram to Server for QOTD
+    SOCKET.sendto(MSG, (IP, PORT))
+
+    # Receive datagram from Server
+    data, addr = SOCKET.recvfrom(512)  # 512 bytes as recommended in RFC
+    print("QOTD: %s" % (data.decode("utf-8")))
+
+
+if __name__ == '__main__':
+    main()
--- QOTD-server.py
+++ QOTD-server.py
@@ -1,54 +1,61 @@
 #!/usr/bin/env python
 
 import socket
+from multiprocessing import Pool
 from datetime import datetime
 
-PORT = 17  # Set default port for QOTD
 
-# Create socket
-try:
-    SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-except socket.error as socket_error:
-    print("Socket failed to create with error %s" % (socket_error))
+def main():
+    PORT = 17  # Set default port for QOTD
 
-# Bind socket
-try:
-    SOCKET.bind(('', PORT))
-except socket.error as socket_error:
-    print("Socket failed to bind with error %s" % (socket_error))
-
-while SOCKET:
-    # Check for new day
-    day = datetime.now()
-
+    # Create socket
     try:
-        print("Ready to receive connections..")
-        # based on RFC for QOTD Protocol the size must not exceed 512 bytes
-        data, addr = SOCKET.recvfrom(512)
-        print("Connection established from %s" % str(addr[0]))
-        # Data received is discarded
+        SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     except socket.error as socket_error:
-        print("Socket failed to receive data with error %s" % (socket_error))
+        print("Socket failed to create with error %s" % (socket_error))
 
+    # Bind socket
     try:
-        # Get QOTD
-        f = open("quotes.txt", "r")
+        SOCKET.bind(('', PORT))
+    except socket.error as socket_error:
+        print("Socket failed to bind with error %s" % (socket_error))
 
-        day_buffer = datetime.now()
-        day_differences = day_buffer - day
-        day_differences = str(day_differences)
-        day_differences = day_differences.split(" ")[0]
-        day_diff_len = len(day_differences)
+    while SOCKET:
+        # Check for new day
+        day = datetime.now()
 
-        if day_diff_len == 1:
-            MSG = f.readline().encode("utf-8")
-        else:
-            i = int(day_differences[0])
-            for x in range(i + 1):
+        try:
+            print("Ready to receive connections..")
+            # based on RFC for QOTD Protocol the size must not exceed 512 bytes
+            data, addr = SOCKET.recvfrom(512)
+            print("Connection established from %s" % str(addr[0]))
+            # Data received is discarded
+        except socket.error as socket_error:
+            print("Socket failed to receive data with error %s" % (socket_error))
+
+        try:
+            # Get QOTD
+            f = open("quotes.txt", "r")
+
+            day_buffer = datetime.now()
+            day_differences = day_buffer - day
+            day_differences = str(day_differences)
+            day_differences = day_differences.split(" ")[0]
+            day_diff_len = len(day_differences)
+
+            if day_diff_len == 1:
                 MSG = f.readline().encode("utf-8")
+            else:
+                i = int(day_differences[0])
+                for x in range(i + 1):
+                    MSG = f.readline().encode("utf-8")
 
-        # Send QOTD to client
-        SOCKET.sendto(MSG, addr)
-        print("Sent Quote to %s" % (str(addr[0])))
-    except socket.error as socket_error:
-        print("Socket failed to send data with error %s" % (socket_error))
+            # Send QOTD to client
+            SOCKET.sendto(MSG, addr)
+            print("Sent Quote to %s" % (str(addr[0])))
+        except socket.error as socket_error:
+            print("Socket failed to send data with error %s" % (socket_error))
+
+
+if __name__ == '__main__':
+    main()
