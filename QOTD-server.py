b5878b93f899326ad07202322e9c9719057f5858
riazufila <46749569+riazufila@users.noreply.github.com>
2020-12-08 12:41:58 +0800

    Functions

--- QOTD-server.py
+++ QOTD-server.py
@@ -5,21 +5,7 @@ import multiprocessing
 from datetime import datetime
 
 
-def socket_creation_bind(PORT):
-    try:
-        SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    except socket.error as socket_error:
-        print("Socket failed to create with error %s" % (socket_error))
-
-    try:
-        SOCKET.bind(('', PORT))
-    except socket.error as socket_error:
-        print("Socket failed to bind with error %s" % (socket_error))
-
-        return SOCKET
-
-
-def socket_listen(SOCKET):
+def socket_operation(SOCKET):
     while SOCKET:
         # Check for new day
         day = datetime.now()
@@ -35,20 +21,7 @@ def socket_listen(SOCKET):
 
         try:
             # Get QOTD
-            f = open("quotes.txt", "r")
-
-            day_buffer = datetime.now()
-            day_differences = day_buffer - day
-            day_differences = str(day_differences)
-            day_differences = day_differences.split(" ")[0]
-            day_diff_len = len(day_differences)
-
-            if day_diff_len == 1:
-                MSG = f.readline().encode("utf-8")
-            else:
-                i = int(day_differences[0])
-                for x in range(i + 1):
-                    MSG = f.readline().encode("utf-8")
+            MSG = get_quote(day)
 
             # Send QOTD to client
             SOCKET.sendto(MSG, addr)
@@ -57,16 +30,43 @@ def socket_listen(SOCKET):
             print("Socket failed to send data with error %s" % (socket_error))
 
 
+def get_quote(day):
+    f = open("quotes.txt", "r")
+
+    day_buffer = datetime.now()
+    day_differences = day_buffer - day
+    day_differences = str(day_differences)
+    day_differences = day_differences.split(" ")[0]
+    day_diff_len = len(day_differences)
+
+    if day_diff_len == 1:
+        MSG = f.readline().encode("utf-8")
+    else:
+        i = int(day_differences[0])
+        for x in range(i + 1):
+            MSG = f.readline().encode("utf-8")
+
+    return MSG
+
+
 def main():
     """ Main function """
     # Variables declaration
     PORT = 17  # Set default port for QOTD
 
     # Create socket and bind
-    SOCKET = socket_creation_bind(PORT)
+    try:
+        SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except socket.error as socket_error:
+        print("Socket failed to create with error %s" % (socket_error))
+
+    try:
+        SOCKET.bind(('', PORT))
+    except socket.error as socket_error:
+        print("Socket failed to bind with error %s" % (socket_error))
 
     # Socket listening
-    socket_listen(SOCKET)
+    socket_operation(SOCKET)
 
 
 if __name__ == '__main__':
