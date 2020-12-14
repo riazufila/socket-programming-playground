983f39e1a33176de0b4221b0f4b085408c4df983
riazufila <46749569+riazufila@users.noreply.github.com>
2020-12-14 17:01:40 +0800

    Encryption

--- QOTD-client.py
+++ QOTD-client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
+from cryptography.fernet import Fernet
 
 
 if __name__ == '__main__':
@@ -8,6 +9,7 @@ if __name__ == '__main__':
     port = 17  # Default QOTD port
     ip_host = b"192.168.12.34"  # Server's ip
     msg = b"Let me in!"
+    sym_key = b'db81nHBk-RcQ_-zSMcWdCEuH53LUX1PFiCA_2R7CKK8='
 
     # Create socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,4 +19,10 @@ if __name__ == '__main__':
 
     # Receive datagram from Server
     data, addr = s.recvfrom(512)  # 512 bytes as recommended in RFC
+
+    # Decrypt the message with the defined key
+    en_type = Fernet(sym_key)
+    de_msg = en_type.decrypt(data)
+
+    # Print out the quote
     print("QOTD: %s" % (data.decode("utf-8")))
--- QOTD-server.py
+++ QOTD-server.py
@@ -3,6 +3,16 @@
 import socket
 import threading
 from datetime import datetime
+from cryptography.fernet import Fernet
+
+
+def encrypt_msg(MSG):
+    # Encrypt with predefined key
+    sym_key = b'db81nHBk-RcQ_-zSMcWdCEuH53LUX1PFiCA_2R7CKK8='
+    en_type = Fernet(sym_key)
+    en_msg = en_type.encrypt(MSG)
+
+    return en_msg
 
 
 def send_msg(SOCKET, MSG, addr):
@@ -30,6 +40,7 @@ def get_quote(day):
 
 
 if __name__ == '__main__':
+
     # Variables declaration
     PORT = 17  # Set default port for QOTD
 
@@ -51,9 +62,11 @@ if __name__ == '__main__':
 
         try:
             print("Ready to receive connections..")
+
             # based on RFC for QOTD Protocol the size must not exceed 512 bytes
             # Data received is discarded
             data, addr = SOCKET.recvfrom(512)
+
             print("Connection established from %s" % str(addr[0]))
         except socket.error as socket_error:
             print("Socket failed to receive data with error %s" % (socket_error))
@@ -62,8 +75,12 @@ if __name__ == '__main__':
             # Get QOTD
             MSG = get_quote(day)
 
+            # Encrypt message with fernet
+            MSG = encrypt_msg(MSG)
+
             # Send quote to client
             # Threading to allow multiple clients at the same time
             threading.Thread(target=send_msg, args=(SOCKET, MSG, addr)).start()
         except socket.error as socket_error:
+
             print("Socket failed to send data with error %s" % (socket_error))
