eaa79743a33c4131eee42c14c953302f9a3c2c86
riazufila <46749569+riazufila@users.noreply.github.com>
2020-12-15 11:54:24 +0800

    6.2 client

---
+++ 6.2/client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import socket
+
+ClientSocket = socket.socket()
+host = '192.168.12.34'
+port = 8888
+
+print("Waiting for connection")
+try:
+    ClientSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+
+Response = ClientSocket.recv(1024)
+print(Response)
+while True:
+    Input = input('Say Something:')
+    ClientSocket.send(str.encode(Input))
+    Response = ClientSocket.recv(1024)
+    print(Response.decode("utf-8"))
+
+ClientSocket.close()
